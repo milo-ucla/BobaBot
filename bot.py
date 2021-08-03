e0216dd0f259bfbe57357d4714424ff7f5b0e7af
milo-ucla <milok23@ucla.edu>
2021-08-03 16:19:06 -0700

    changed function setup - split get_quote() into two helpers. Added TODO section. Fixed random.choice() bug for boba? handling.

--- bot.py
+++ bot.py
@@ -11,23 +11,35 @@ nlp = spacy.load("en_core_web_sm")
 client = discord.Client()
 load_dotenv()
 
+'''
+TO DO
+NLP: The part-of-speech is course right now, only identifying NOUN or not
+    it should instead be more fine, only replacing parts of speech that can be "boba". 
+    Once this is done, the tog bool should be removed or adjusted.
+
+DB: When a user logs boba
+'''
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
     json_data = json.loads(response.text)
     quote = json_data[0]['q']
-    doc = nlp(quote)
-    message = quote
+    print(quote)
+    return [quote, json_data[0]['a']]
+
+def bobafy_quote(input: str) -> str:
+    message = input
+    doc = nlp(input)
     first = True
     tog = True
-    print(quote)
+    replaced = []
     for token in doc:
         if token.pos_ == "NOUN" and tog:
             if(first):
-                message = message.replace(str(token), "Boba")
-                message = message.replace(str(token).lower(), "boba")
+                replaced.append([str(token), "Boba"])
+                replaced.append([str(token).lower(), "boba"])
                 first = False
             else:
-                message = message.replace(str(token), "boba")
+                replaced.append([str(token), "boba"])
         
         if(token.pos_ == "PUNCT" and str(token) != ','):
             first = True
@@ -36,7 +48,12 @@ def get_quote():
             
         if(token.pos_ == "NOUN"):
             tog = not tog
+    replaced.sort()
+    for pair in replaced:
+        message = message.replace(pair[0], pair[1])
     return(message + " - BobaBot")
+
+
 #to fix a bug where substrings of longer words are replaced with "boba" (like act -> boba, character -> charbobaer ), add 
 #all of the nouns to a list and then sort by size before replacing
 
@@ -51,19 +68,23 @@ async def on_message(message):
         return
 
     if message.content.startswith('!quote'):
-        my_message = get_quote()
-        await message.channel.send(my_message)
+        quote = get_quote()
+        my_message = bobafy_quote(quote[0])
+        fullquote = quote[0] + " - " + quote[1]
+        await message.channel.send(my_message + '\n||' + fullquote +'||')
 
     if message.content.startswith('!boba?'):
         my_message = "I can't say, not until Milo codes this bot better"
-        if(random.choice([0,1]) == 1):
+        yn = random.choice([True, False])
+        print(yn)
+        if(yn):
             my_message = "Yes, go get some boba!"
         else:
             my_message = "Nah, not today"
         await message.channel.send(my_message)
 
     elif message.content.startswith('!boba'):
-        my_message = "I love boba!"
+        my_message = "I love boba!\n{This feature is under development}"
         await message.channel.send(my_message)
 
 client.run(os.getenv('TOKEN'))
