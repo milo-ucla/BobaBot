3c9f2039af8ef1e679055ffe209e2f463b72e9db
Colin Curtis <colinpcurtis826@ucla.edu>
2021-08-09 20:20:27 -0700

    got multi-server updates to work

--- bot.py
+++ bot.py
@@ -18,8 +18,6 @@ mongoclient = MongoClient(os.getenv("MONGO_URL"))
 
 db = mongoclient.boba_db
 
-boba_count_db = db.boba_count
-
 
 def get_quote():
     response = requests.get("https://zenquotes.io/api/random")
@@ -53,7 +51,7 @@ def bobafy_quote(message):
     replaced.sort()
     for pair in replaced:
         message = message.replace(pair[0], pair[1])
-    return(message + " - BobaBot")
+    return(message + " - BobaBot")    
 
 
 #to fix a bug where substrings of longer words are replaced with "boba" (like act -> boba, character -> charbobaer ), add 
@@ -66,40 +64,86 @@ async def on_ready():
 
 
 def get_count(user):
+    collection_name = db.list_collection_names()[0]
+    collection = db.collection_name
     user_query = {
             "user": user,
     }
-    query_user = boba_count_db.find_one(user_query)
+    query_user = collection.find_one(user_query)
     if query_user is None:
         return 0
     else:
         return query_user["boba_count"]
 
-def update_count(user, increment = 1):
+
+def update_count(collection, user, increment=1):
     user_query = {
             "user": user,
     }
-    query_user = boba_count_db.find_one(user_query)
+    query_user = collection.find_one(user_query)
     if query_user is None:
         count = increment
         user_query["boba_count"] = count
-        boba_count_db.insert_one(user_query)
+        collection.insert_one(user_query)
     else:
         filter = { 'user': user }
         newvalues = { "$set": { 'boba_count': query_user["boba_count"] + increment } }
-        boba_count_db.update_one(filter, newvalues) 
+        collection.update_one(filter, newvalues) 
         count = query_user["boba_count"] + increment
     return count
+
+
+def update_across_databases(user, value=1):
+    collections = db.list_collection_names()
+    count = None
+    for collection_name in collections:
+        collection = db[str(collection_name)]
+        count = update_count(collection, user, value)
+    return count
+
+
+async def handle_boba(message):
+    username = str(message.author).split("#")[0]
+    word_list = message.content.split(" ")
+    if len(word_list) > 2 or word_list[-1] == "help":
+        my_message = f"Use the boba count with 1 argument.\nType `count` to get your boba count, type `integer` to add to your boba count (default: 1).\nFor more information, try !boba info"
+        await message.channel.send(my_message)
+    elif word_list[-1] == "count":
+        # user calls !boba count to get their boba count
+        count = get_count(username)
+        my_message = f"{username}'s boba count is {count}"
+        await message.channel.send(my_message)
+    elif word_list[-1] == "info":
+        my_message = "https://github.com/milo-ucla/BobaBot/blob/4f25e9e1472c81d0dcc14efe334f653d9a738e41/README.md"
+        await message.channel.send(my_message)
+    elif len(word_list) == 2:
+        incr = 0
+        my_message = "Error: unhandled exception"
+        try:
+            incr = int(word_list[-1])
+            count = update_across_databases(username, incr)
+            my_message = f"{username}'s boba count is now {count}"
+        except:
+            my_message = "Error: second arg invalid. Try `!boba help` for more information."
+        # user just calls !boba to increment count
+        await message.channel.send(my_message)
+    elif len(word_list) == 1:
+        # user just calls !boba to increment count
+        count = update_across_databases(username)
+        my_message = f"{username}'s boba count is now {count}"
+        await message.channel.send(my_message)
+
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    channel = message.channel.id 
-    # TODO: make the database corespond to the server
+    channel = message.guild
 
-    username = str(message.author).split("#")[0]
+    if str(channel) not in db.list_collection_names():
+        db.create_collection(str(channel))
+    
     if message.content.startswith('!boba quote'):
         quote = get_quote()
         my_message = bobafy_quote(quote[0])
@@ -116,32 +160,7 @@ async def on_message(message):
         await message.channel.send(my_message)
 
     elif message.content.startswith('!boba'):
-        word_list = message.content.split(" ")
-        if len(word_list) > 2 or word_list[-1] == "help":
-            my_message = f"Use the boba count with 1 argument.\nType `count` to get your boba count, type `integer` to add to your boba count (default: 1).\nFor more information, try !boba info"
-            await message.channel.send(my_message)
-        elif word_list[-1] == "count":
-            # user calls !boba count to get their boba count
-            count = get_count(username)
-            my_message = f"{username}'s boba count is {count}"
-            await message.channel.send(my_message)
-        elif word_list[-1] == "info":
-            my_message = "https://github.com/milo-ucla/BobaBot/blob/4f25e9e1472c81d0dcc14efe334f653d9a738e41/README.md"
-            await message.channel.send(my_message)
-        elif len(word_list) == 2:
-            incr = 0
-            my_message = "Error: unhandled exception"
-            try:
-                incr = int(word_list[-1])
-                count = update_count(username, incr)
-                my_message = f"{username}'s boba count is now {count}"
-            except:
-                my_message = "Error: second arg invalid. Try `!boba help` for more information."
-            # user just calls !boba to increment count
-            await message.channel.send(my_message)
-        elif len(word_list) == 1:
-            # user just calls !boba to increment count
-            count = update_count(username)
-            my_message = f"{username}'s boba count is now {count}"
-            await message.channel.send(my_message)
+        await handle_boba(message)
+
+
 client.run(os.getenv('TOKEN'))
